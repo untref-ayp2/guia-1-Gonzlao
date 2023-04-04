e02493bb3425eb730e04e8f3ab5e97406601dd12
Gonzalo Giuffre <gonzalogiuffre@MacBook-Pro-de-Gonzalo.local>
2023-04-04 13:18:16 -0300

    Ejercicios de Subsecuencia de Suma Máxima

--- 01-subsumamax/subsumamax.go
+++ 01-subsumamax/subsumamax.go
@@ -2,19 +2,20 @@ package subsumamax
 
 // Dado un arreglo devuelve la posicion inicial, la posición final y el valor
 // de la subsecuencia cuya suma es máxima dentro del arreglo original
+// el orden de la función SubsecuenciaSumaMaxima es de O(n^2)
 func SubsecuenciaSumaMaxima(arreglo []int) (int, int, int) {
 	sumaMaxima := 0
 	posInicial, posFinal := -1, -1
-	for i := 0; i < len(arreglo); i++ {
-		sumaLocal := 0
-		for j := i; j < len(arreglo); j++ {
-			sumaLocal += arreglo[j]
-			if sumaLocal > sumaMaxima {
-				sumaMaxima = sumaLocal
-				posInicial = i
-				posFinal = j
+	for i := 0; i < len(arreglo); i++ { // 1+n*(n+1)  es equivalente a n^2
+		sumaLocal := 0                      // es 1
+		for j := i; j < len(arreglo); j++ { // 1+n*(1+1) es equivalente a n
+			sumaLocal += arreglo[j]     // es 1
+			if sumaLocal > sumaMaxima { // 1+1+1 es equivalente a 1
+				sumaMaxima = sumaLocal // es 1
+				posInicial = i         // es 1
+				posFinal = j           // es 1
 			}
 		}
 	}
-	return sumaMaxima, posInicial, posFinal
+	return sumaMaxima, posInicial, posFinal // es 1
 }
